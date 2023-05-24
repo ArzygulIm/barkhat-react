61ea8dbba10cc7bcb5e676c9a96120f197294fd7
Пользователь <arzygul.imankulova.kk@gmail.com>
2023-05-25 03:46:32 +0600

    no client

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {LINES_API} from "../../config";
 import "./style.css"
 
 const Main = () => {
-    console.log(LINES_API)
-    console.log(process.env.REACT_APP_API)
     return (
         <div className={'container main__container'}>
             <Link to={'/admin'} className={"main__links"}>Админ</Link>
             <Link to={'/reports'} className={"main__links"}>Отчеты</Link>
-            <Link to={'/client'} className={"main__links"}>Клиент</Link>
+            {/*<Link to={'/client'} className={"main__links"}>Клиент</Link>*/}
         </div>
     );
 };
